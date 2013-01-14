3ea4b9d1884350eb285c71e197d6a468ab624ce9
Richard Woodbury <rpwoodbu@mybox.org>
2013-01-13 20:48:26 -0500

    Add problem 15.

---
+++ problem_15.go
@@ -0,0 +1,58 @@
+package projecteuler
+
+/*
+  http://projecteuler.net/problem=15
+
+  Starting in the top left corner of a 2x2 grid, there are 6 routes (without backtracking) to the bottom right corner.
+
+  [See link above for depictive graphic.]
+
+  How many routes are there through a 20x20 grid?
+*/
+
+func GridRouteCounter(x, y int) int64 {
+  // Add one because we're interested in intersections, not cells.
+  cache := make([][]int64, x+1)
+  for i := range cache {
+	cache[i] = make([]int64, y+1)
+  }
+
+  // Seed the cache with some initial bottom-right values:
+  //
+  //       * 1
+  //       |
+  // 1 *---+ 0
+  cache[x][y-1] = 1
+  cache[x-1][y] = 1
+
+  return GridRouteLookup(0, 0, cache)
+}
+
+func GridRouteLookup(x, y int, cache [][]int64) int64 {
+  routes := cache[x][y]
+  if routes != 0 {
+	return routes
+  }
+
+  sizeX := len(cache)
+  sizeY := len(cache[0])
+
+  if x+1 < sizeX {
+	routes += GridRouteLookup(x+1, y, cache)
+  }
+
+  if y+1 < sizeY {
+	routes += GridRouteLookup(x, y+1, cache)
+  }
+
+  if routes == 0 {
+	panic("Found point with no routes.")
+  }
+
+  cache[x][y] = routes
+  return routes
+}
+
+func Problem15() int64 {
+  return GridRouteCounter(20, 20)
+}
---
+++ problem_15_test.go
@@ -0,0 +1,12 @@
+package projecteuler
+
+import "testing"
+
+func TestGridRouteCounter(t *testing.T) {
+	AssertInt64(GridRouteCounter(2, 2), 6, t)
+	AssertInt64(GridRouteCounter(3, 3), 20, t)
+}
+
+func TestProblem15(t *testing.T) {
+	AssertInt64(Problem15(), 137846528820, t)
+}
