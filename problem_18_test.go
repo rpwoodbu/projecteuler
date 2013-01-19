5a00a8186cbf50baa7a6e860e589e1ba614d2883
Richard Woodbury <rpwoodbu@mybox.org>
2013-01-19 16:09:07 -0500

    Add problem 18.

---
+++ problem_18_test.go
@@ -0,0 +1,18 @@
+package projecteuler
+
+import "testing"
+
+func TestMaxPath(t *testing.T) {
+	triangle := [][]int{
+		[]int{3},
+		[]int{7, 4},
+		[]int{2, 4, 6},
+		[]int{8, 5, 9, 3},
+	}
+
+	AssertInt(MaxPath(triangle), 23, t)
+}
+
+func TestProblem18(t *testing.T) {
+	AssertInt(Problem18(), 1074, t)
+}
